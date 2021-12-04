dbd61f3ff6a44747ba359b61b36c76a46d9c572a
aanper <mail@s3f.ru>
2021-12-04 21:24:39 +0300

    daytime render

--- led/led.ino
+++ led/led.ino
@@ -12,38 +12,118 @@ void setup() {
   strip.clear();
 
   Serial.begin(115200);
+  Serial.setTimeout(100);
 
   pinMode(13, OUTPUT);
 }
 
 float fcos(float x) {
-  x /= 32768;
   float tp = 1./(2.*M_PI);
   x *= tp;
   x -= (.25) + floor(x + (.25));
   x *= (16.) * (abs(x) - (.5));
-  #if EXTRA_PRECISION
-  x += T(.225) * x * (std::abs(x) - T(1.));
-  #endif
   return x;
 }
 
-void render(uint32_t t_x, uint16_t i_x, vec3* out_color) {
-  float x = (float)i_x / 32768;
-  float t = (float)t_x / 32;
+void mix(vec3* out_color, vec3* color_0, vec3* color_1, float value) {
+  if(value > 1.) value = 1.;
+  if(value < 0.) value = 0.;
+  // int32_t i_value = (int32_t)value * 250;
   
-  // out_color->g = 50 * (1. + fcos((float)t/4000 + x * 0));
-  out_color->b = 90 * (1. + fcos(32768 * ((float)t/2000 + x * 6)));
-  out_color->r = 255 - out_color->b;
+  float c_value = 1. - value;
+  
+  out_color->r = (uint8_t)((uint16_t)color_0->r * c_value + (uint16_t)color_1->r * value) >> 0;
+  out_color->g = (uint8_t)((uint16_t)color_0->g * c_value + (uint16_t)color_1->g * value) >> 0;
+  out_color->b = (uint8_t)((uint16_t)color_0->b * c_value + (uint16_t)color_1->b * value) >> 0;
+}
+
+vec3 main_color = {0, 0, 0};
+
+vec3 sun = {0xff, 0xd9, 0x25};
+vec3 sky = {0x3e, 0x9e, 0xe2};
+vec3 cloud = {0xff, 0xff, 0xff};
+// vec3 cloud = {0xff, 0x00, 0x00};
+vec3 grass_0 = {0x45, 0xc4, 0x00};
+vec3 grass_1 = {0x07, 0x29, 0x03};
+
+#define CLOUD_COUNT 3
+
+float cloud_position[CLOUD_COUNT] = {0.5, 0.8, 0.3};
+float cloud_size[CLOUD_COUNT] = {0.03, 0.02, 0.025};
+float cloud_speed[CLOUD_COUNT] = {0.01, 0.005, 0.007};
+
+float frac(float v) {
+  return v - floor(v);
+}
+
+void render_day(float t, float x, vec3* out_color) {
+  bool is_ground = (x > 0.68 && x < 0.84) || (x < 0.34);
+  // bool is_ground = false;
+  
+  if(is_ground) {
+    vec3 grass;
+    mix(&grass, &grass_0, &grass_1, fcos(x * 200 + t * 0.3) * 3);
+    memcpy(out_color, &grass, sizeof(vec3));
+  } else {
+    memcpy(out_color, &sky, sizeof(vec3));
+    for(size_t i = 0; i < CLOUD_COUNT; i++) {
+      float cloud_x = frac(cloud_position[i] + frac(t * cloud_speed[i]));
+      mix(out_color, out_color, &cloud,
+        (cloud_size[i] - abs(x - cloud_x)) * 200
+      );
+    }
+
+    float sun_x = frac(t * 0.02);
+    float sun_size = 0.05;
+    
+    mix(out_color, out_color, &sun,
+      (sun_size - abs(x - sun_x)) * 100
+    );
+  }
+}
+
+void render_waves(float t, float x, vec3* out_color) {
+  /*out_color->b = 90 * (1. + fcos((t*6/6 + x * 6)));
+  out_color->r = 255 - out_color->b;*/
+}
+
+void render(float t, float x, vec3* out_color) {
+  render_day(t, x, out_color);
+}
+
+void user_input() {
+  String s = Serial.readStringUntil('\n');
+  if(s == NULL) return;
+
+  vec3* target = NULL;
+  switch(s[0]) {
+    case '#':
+      target = &main_color;
+    break;
+    case 'g':
+      target = &grass_0;
+    break;
+    case 's':
+      target = &sky;
+  }
+
+  if(target != NULL) {
+    Serial.println(s);
+    sscanf(&s.c_str()[1], "%2X%2X%2X", &(target->r), &(target->g), &(target->b));
+  } else {
+    Serial.println("enter <>hex");
+  }
 }
 
 void loop() {
-  // strip.clear();
-  uint32_t t = (uint32_t)millis() * 32.768 * 2.5;
+  // user_input();
+  
+  strip.clear();
+  float t = (float)millis() / 1000;
   // Serial.println(t);
-  for(uint16_t n = NUM_LEDS; n > 0; n--) {
+  for(uint16_t n = 0; n < NUM_LEDS; n++) {
     vec3 out_color = {0, 0, 0};
-    render(t, ((uint32_t)n * 32768 / NUM_LEDS), &out_color);
+    render(t, ((float)n / NUM_LEDS), &out_color);
     strip.setPixelColor(n, out_color.r, out_color.g, out_color.b);
     digitalWrite(13, n < NUM_LEDS/2 ? HIGH : LOW);
   }
